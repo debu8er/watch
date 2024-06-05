b31edef2cf26e09e4c45afe6eceabb42ffcb3906
debu8er <121763963+debu8er@users.noreply.github.com>
2024-06-06 03:05:56 +0330

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,20 @@
+import os
 import pymongo
+import requests
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+
+BASE_URL = os.getenv('BASE_URL')
+MY_DB = os.getenv('MY_DB')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+
 
 def initialize_mongo_collection(domain):
-    myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
-    mydb = myclient["mydatabase"]
-    mycol = mydb[domain.split(".")[0]]
+    myclient = pymongo.MongoClient(BASE_URL)
+    mydb = myclient[MY_DB]
+    mycol = mydb[domain]
     return mycol
 
 def fetch_domains_from_mongodb():
@@ -13,3 +24,12 @@ def fetch_domains_from_mongodb():
     domain_names = domains_collection.distinct("domain")
     client.close()
     return domain_names
+
+def send_data_to_discord(sub):
+    webhook_url = WEBHOOK_URL
+    data = {
+        "username": "Webhook Name",
+        "content": f"{sub}"
+    }
+
+    response = requests.post(webhook_url, json=data)
